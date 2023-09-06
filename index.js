dec11142b2986c71a2cac90af307a940594c1df3
piumikaDil <piumikaDil@gmail.com>
2023-09-06 12:48:40 +0530

    DELETE requuest

--- index.js
+++ index.js
@@ -16,6 +16,29 @@ app.post("/posts", (req, res) => {
    res.send(body)
 })
 
+app.get("/posts/:id", (req, res) => {
+    const postId = req.params.id
+    console.log(postId);
+    const post = posts.find((post)=>post.id === postId)
+    if(post){
+        res.json(post)
+    }else{
+        res.status(404)
+    }
+})
+
+app.delete("/posts/:id", (req, res) => {
+    const postID = req.params.id;
+    const post = posts.findIndex((post) => post.id === postID);
+    if (post !== -1) {
+        const deletePost = posts.splice(post, 1);
+        res.json({ message: "Deleted" });
+    } else {
+        res.send(404).json({ message: "Post Not Found" });
+    }
+});
+
+
 app.listen(PORT,()=>{
     console.log("server is running");
 })
